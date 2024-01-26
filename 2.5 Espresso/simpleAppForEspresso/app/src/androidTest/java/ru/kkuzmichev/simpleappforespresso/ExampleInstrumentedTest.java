2acf1ae1d1af11621da8b016e61f7a789769d8fd
Krideinside <kride2009@yandex.ru>
2024-01-26 12:49:23 +0300

    fixed

--- 2.5 Espresso/simpleAppForEspresso/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ 2.5 Espresso/simpleAppForEspresso/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -1,14 +1,24 @@
 package ru.kkuzmichev.simpleappforespresso;
 
-import android.content.Context;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Context;
+import android.view.View;
+
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static java.util.regex.Pattern.matches;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,5 +32,18 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
+    }
+
+    @Test
+    public void testName() {
+
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment")
+                )
+        );
     }
 }
